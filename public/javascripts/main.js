dd725eea3c68ccf60f5bddb3ffe5ae55f6081034
mikemjharris <mike.mj.harris@gmail.com>
2017-09-30 18:37:36 +0100

    initial pwa

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,10 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('./service-worker.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
+
+
 (function ( $ ) {
 
   $(function() {
---
+++ public/service-worker.js
@@ -0,0 +1,53 @@
+var cacheName = 'mikemjharris-blog-2';
+var filesToCache = [
+  "/",
+  "/api/posts",
+  "/dist/style.css",
+  "/jquery/dist/jquery.js",
+  "/handlebars/dist/handlebars.js",
+  "/dist/templates.js",
+  "/dist/main.js",
+  "http://fonts.googleapis.com/css?family=Spinnaker",
+  "http://fonts.googleapis.com/css?family=Inconsolata",
+  "http://fonts.googleapis.com/css?family=Jura"
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      }
+    )
+  );
+});
