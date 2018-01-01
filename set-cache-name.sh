14b80c7f0b601617f944621016efdd538fd96e42
mikemjharris <mike.mj.harris@gmail.com>
2018-01-01 11:48:42 +0000

    update cache script

--- set-cache-name.sh
+++ set-cache-name.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO don't commit the change and run this on build time.# TODO don't commit the change and run this on build time.# TODO don't commit the change and run this on build time.
+
 COMMIT=`git rev-parse --short head`
 
 less ./public/compile/service-worker.js | sed -e s/mikemjharris-blog-.*/mikemjharris-blog-$COMMIT\'/g > ./public/service-worker.js
