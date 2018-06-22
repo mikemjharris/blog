f86a4a9aa07ec2c9a7f54f4177e94325233410eb
Mike Harris <hello@mikemjharris.com>
2018-06-22 18:36:00 +0100

    add tests

--- server/tests/helpers.test.js
+++ server/tests/helpers.test.js
@@ -4,3 +4,13 @@ test('only gets html files from path', () => {
     const posts = postHelpers.getPostsFromPath('./server/tests/fixtures/test-posts/');
     expect(posts.length).toBe(1);
 });
+
+test('meta data is parsed correctly', () => {
+    const posts = postHelpers.getPostsFromPath('./server/tests/fixtures/test-posts/');
+    expect(posts[0].title).toEqual('Test post');
+    expect(posts[0].searchtitle).toEqual('test-post');
+    expect(posts[0].date).toEqual('24 Dec 2015');
+    expect(posts[0].intro).toEqual('Test post intro');
+    expect(posts[0].author).toEqual('Mike Harris');
+    expect(posts[0].category).toEqual('thoughts');
+});
