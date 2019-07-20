418b640157ceee9f2ec0f3e6a3eb7e9743857b47
Mike Harris <hello@mikemjharris.com>
2019-07-20 07:59:00 +0100

    Add includes helper

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -10,7 +10,17 @@
               } else {
                   return options.fn(this);
               }
-            }
+            },
+            includes: function(lvalue, rvalue, options) {
+              if (arguments.length < 3) {
+                  throw new Error( 'Handlebars Helper equal needs 2 parameters' );
+                }
+              if ( lvalue.indexOf(rvalue) == -1) {
+                  return options.inverse(this);
+              } else {
+                  return options.fn(this);
+              }
+            },
         };
 
         if ( Handlebars && typeof Handlebars.registerHelper === 'function' ) {
